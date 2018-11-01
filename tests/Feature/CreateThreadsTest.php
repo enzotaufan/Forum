092e2fc223990ed1efb4d18eb8d06e02ef0688c0
vagrant <vagrant@homestead>
2018-11-01 03:45:34 +0000

    15 %

---
+++ tests/Feature/CreateThreadsTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class CreateThreadsTest extends TestCase
+{
+	use DatabaseMigrations;
+
+	/** @test */
+	function guests_may_not_create_threads()
+	{
+		$this->withExceptionHandling();
+   			
+   			$this->get('/threads/create')
+   			->assertRedirect('/login');
+
+		
+   			$this->post('/threads')
+   			->assertRedirect('/login');
+
+	}	
+
+
+
+    /** @test */
+    function an_authenticated_user_can_create_forum_threads()
+    {
+		$this->signIn();
+
+		$thread = make('App\Thread');
+
+		$this->post('/threads', $thread->toArray());	
+
+		$this->get($thread->path())
+			->assertSee($thread->title)
+			->assertSee($thread->body);
+    }
+    
+}
